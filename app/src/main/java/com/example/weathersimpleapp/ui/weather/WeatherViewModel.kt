3968f9ede0eb6f0294b9009a37f91be7336158c2
JY-DEV <kjy0302014@gmail.com>
2021-04-22 22:41:52 +0900

    Manifest 불필요한 요소 제거

--- app/src/main/java/com/example/weathersimpleapp/ui/weather/WeatherViewModel.kt
+++ app/src/main/java/com/example/weathersimpleapp/ui/weather/WeatherViewModel.kt
@@ -4,5 +4,5 @@ import androidx.lifecycle.ViewModel
 import com.example.weathersimpleapp.data.repository.WeatherRepository
 
 class WeatherViewModel(private val weatherRepository: WeatherRepository) : ViewModel() {
-    
+
 }
