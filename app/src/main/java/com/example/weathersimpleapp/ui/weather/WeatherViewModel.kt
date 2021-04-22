008798bd375fbb51b983bf92e99de13ab9c459b5
JY-DEV <kjy0302014@gmail.com>
2021-04-22 23:01:39 +0900

    WeatherViewModel 생성자에 City 추가 & getWeather 함수 추가

--- app/src/main/java/com/example/weathersimpleapp/ui/weather/WeatherViewModel.kt
+++ app/src/main/java/com/example/weathersimpleapp/ui/weather/WeatherViewModel.kt
@@ -1,8 +1,27 @@
 package com.example.weathersimpleapp.ui.weather
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.weathersimpleapp.data.repository.WeatherRepository
+import com.example.weathersimpleapp.models.city.City
+import com.example.weathersimpleapp.models.response.WeatherResponse
+import com.example.weathersimpleapp.models.weather.Weather
 
-class WeatherViewModel(private val weatherRepository: WeatherRepository) : ViewModel() {
+class WeatherViewModel(val city: City ,private val weatherRepository: WeatherRepository) : ViewModel() {
 
+    private val _weather = MutableLiveData<WeatherResponse>()
+    val weather: LiveData<WeatherResponse> = _weather
+
+    init {
+        city.let {
+            weatherRepository.getWeatherFromCityIdService(it.id)
+                .subscribe((_weather::setValue), { throwable ->
+                    throwable.printStackTrace()
+                })
+        }
+    }
+    fun getWeather() {
+
+    }
 }
