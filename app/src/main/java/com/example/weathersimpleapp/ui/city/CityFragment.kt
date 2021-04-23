ec3d344fd2f8ea4d2ca4d701bd63b07f0ca8e118
Jykim <kjy@geosoft.co.kr>
2021-04-23 13:06:18 +0900

    CityRecyclerView Adapter 연결

--- app/src/main/java/com/example/weathersimpleapp/ui/city/CityFragment.kt
+++ app/src/main/java/com/example/weathersimpleapp/ui/city/CityFragment.kt
@@ -7,11 +7,16 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import com.example.weathersimpleapp.R
 import com.example.weathersimpleapp.databinding.FragmentCityBinding
+import com.example.weathersimpleapp.models.city.City
 import com.example.weathersimpleapp.ui.base.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class CityFragment : BaseFragment<FragmentCityBinding>(R.layout.fragment_city) {
-    private val cityViewModel : CityViewModel by viewModel()
+    private val cityViewModel: CityViewModel by viewModel()
+    private lateinit var cityAdapter: CityAdapter
+    private val gotoWeatherPage : (City) -> Unit = {
+        CityFragmentDirections.actionFragmentCityToFragmentWeather(it).navigate()
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,16 +25,15 @@ class CityFragment : BaseFragment<FragmentCityBinding>(R.layout.fragment_city) {
         return binding {
             lifecycleOwner = this@CityFragment
             viewModel = cityViewModel
+            cityRecyclerView.adapter = CityAdapter(gotoWeatherPage).apply {
+                cityAdapter = this
+            }
             observeData()
         }.root
     }
 
-    private fun observeData(){
-        cityViewModel.cityList.observe(viewLifecycleOwner , Observer {
-            it.forEach {
-                println("name:${it.name}")
-            }
-        })
+    private fun observeData() {
+        cityViewModel.cityList.observe(viewLifecycleOwner, Observer(cityAdapter::setCityList))
     }
 
 }
