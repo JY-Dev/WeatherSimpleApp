bd7315eca2f42e36ec8850b4c199607682e6076d
Jykim <kjy@geosoft.co.kr>
2021-04-23 13:49:29 +0900

    WeatherService Get Url 명시

--- app/src/main/java/com/example/weathersimpleapp/data/network/weather/WeatherService.kt
+++ app/src/main/java/com/example/weathersimpleapp/data/network/weather/WeatherService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherService {
-    @GET
+    @GET("/data/2.5/weather")
     fun getWeatherFromCityId(@Query("id") cityId : Int,
                              @Query("appid") apiKey : String = BuildConfig.WeatherApiKey) : Single<WeatherResponse>
 }
